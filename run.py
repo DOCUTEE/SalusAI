55502fa11360d2d3b57fc44bcba74a6a0f20a8ec
DOCUTEE <quangforwork1203@gmail.com>
2025-04-06 07:21:05 +0700

    first commit

---
+++ run.py
@@ -0,0 +1,10 @@
+from app import create_app
+from flask_cors import CORS
+
+
+app = create_app()
+
+CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
+
+if __name__ == '__main__':
+    app.run(debug=True,host="192.168.124.254",port=8080)
