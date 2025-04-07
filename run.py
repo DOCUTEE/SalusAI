b71673e0ebb29e169c4a7263467fe29e52a02cad
DOCUTEE <quangforwork1203@gmail.com>
2025-04-07 15:08:26 +0700

    Dynamic local ip

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app
 from flask_cors import CORS
+import socket
 
 
 app = create_app()
@@ -7,4 +8,6 @@ app = create_app()
 CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
 
 if __name__ == '__main__':
-    app.run(debug=True,host ="192.168.124.254",port=8080)
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    app.run(debug=True, host=local_ip, port=8080)
